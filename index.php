1b002a8690247b32add50a16556c567c8de52988
Suraaj <navdekarsuraaj3391@gmail.com>
2025-11-20 14:28:37 +0530

    favicon

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ header("Pragma: no-cache");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="logo.png">
     <title>Hotel Woodz</title>
 </head>
 
